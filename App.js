0dbed80daf56a50837af75628074156a7d63151a
marasanz-unizar <839385@unizar.es>
2024-02-28 11:29:16 +0100

    Configuración y instalación

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>LoveRoom</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LoveRoomReact
+
 Desarrollo de LoveRoom para Android e iOS en React Native
+
+Instalación React:
+
+Instalar node con su instalador
+
+sudo npm i -g expo-cli (está medio obsoleto pero funciona, ahora no se usa instalación global)
+
+descargar extensiones de react en vscode
+
+instalar android debugger bridge (adb)
+
+iniciar un emulador del android studio
+
+npx expo start
