e13f24c6d5f0f68410c1bc14e10d7decc51860ce
Andreiiii19 <dumbravaandrei5@gmail.com>
2024-03-04 17:16:47 +0100

    Eslint configurado y pequeñas pruebas de constantes del sistema usado

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar"
+import Constants from "expo-constants"
+import { StyleSheet, Text, View } from "react-native"
+import * as Device from 'expo-device';
 
 export default function App() {
+  const [deviceName, setDeviceName] = useState('');
+
+  useEffect(() => {
+    async function getDeviceName() {
+      const modelName = await Device.modelName;
+      setDeviceName(modelName);
+    }
+
+    getDeviceName();
+  }, []);
+
+  const platformMessage = Constants.platform.ios ? `Estoy en iOS, dispositivo ${deviceName}` : `Estoy en Android, dispositivo ${deviceName}`;
+
   return (
     <View style={styles.container}>
-      <Text>LoveRoom</Text>
+      <Text>{platformMessage}</Text>
       <StatusBar style="auto" />
     </View>
   );
