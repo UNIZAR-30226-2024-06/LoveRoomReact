06c50c1e7827edcb231cff5bbc0591bbac3ec009
Andreiiii19 <dumbravaandrei5@gmail.com>
2024-03-29 16:19:40 +0100

    Todo correcto

--- src/components/StackNavigator.jsx
+++ src/components/StackNavigator.jsx
@@ -6,32 +6,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import BottomTab from './BottomTab';
 import NotRegisteredScreen from '../screens/NotRegisteredScreen';
 import RegisterScreen from '../screens/RegisterScreen';
-<<<<<<< Updated upstream
 import ChangePasswdScreen from '../screens/ChangePasswdScreen';
 import ChangePasswd2Screen from '../screens/ChangePasswd2Screen';
-=======
-import { useWindowDimensions } from 'react-native';
 import Drawer from './Drawer';
->>>>>>> Stashed changes
+import { useWindowDimensions } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
-<<<<<<< Updated upstream
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Account" component={BottomTab} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="NotRegistered" component={NotRegisteredScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ChangePassword" component={ChangePasswdScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ChangePassword2" component={ChangePasswd2Screen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-=======
     const dimensions = useWindowDimensions();
     const isLandscape = dimensions.width > dimensions.height;
 
@@ -39,11 +21,12 @@ export default function StackNavigator() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Account" component={isLandscape ? Drawer : BottomTab} options={{ headerShown: false }}/>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Register" component={RegisterScreen} />
-                <Stack.Screen name="NotRegistered" component={NotRegisteredScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="NotRegistered" component={NotRegisteredScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="ChangePassword" component={ChangePasswdScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="ChangePassword2" component={ChangePasswd2Screen} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
       );
 }
->>>>>>> Stashed changes
